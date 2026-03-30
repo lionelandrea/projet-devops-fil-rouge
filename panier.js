26d476396422bfbfc4719b637898b45a6d78755d
lionelandrea <lionelandreaboscolo@gmail.com>
2026-03-30 15:14:30 +0100

    Sabotage du panier pour verifier le blocage du livrable

--- panier.js
+++ panier.js
@@ -1,6 +1,6 @@
 function calculerTotal(prix) {
   if (prix > 100) {
-    return prix * 0.8;  // Appliquer 20% de réduction (multiplier par 0.8)
+    return prix * 0.5;  // Appliquer 20% de réduction (multiplier par 0.8)
   }
   return prix;
 }
